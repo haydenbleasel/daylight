513d4b66e96dcc8848f43702f313110c9bf308ec
Hayden Bleasel <hello@haydenbleasel.com>
2022-07-08 21:21:05 +1000

    Attempt disabling Chrome sandbox

--- utils/browser.ts
+++ utils/browser.ts
@@ -2,7 +2,9 @@ import type { Browser } from 'puppeteer-core';
 import chrome from 'chrome-aws-lambda';
 
 export const createBrowser = async (): Promise<Browser> => {
-  const browser = chrome.puppeteer.launch();
+  const browser = chrome.puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
 
   return browser;
 };
